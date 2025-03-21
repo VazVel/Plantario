55fc40d0af5b7d1015efba80fd8a36883e5c0466
VazVel <152049637+VazVel@users.noreply.github.com.>
2025-03-21 09:35:21 -0600

    Commit cambio de framework a next.js

---
+++ plantario/pages/api/planta.js
@@ -0,0 +1,30 @@
+import { ejecutarConsulta } from '../../lib/db';
+
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    try {
+      const consultaSQL = 'SELECT * FROM obtener_plantas_activas();';
+      const plantas = await ejecutarConsulta(consultaSQL);
+      res.status(200).json(plantas);
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).json({ message: 'Error al obtener las plantas' });
+    }
+  } 
+  else if (req.method === 'POST') {
+    try {
+      const { nombrePlanta, regadoDias, fertilizadoDias, podadoDias, regadoComposta, descripcionPlanta, status } = req.body;
+      const consultaSQL = `SELECT insertar_planta(
+        '${nombrePlanta}', ${regadoDias}, ${fertilizadoDias}, ${podadoDias}, ${regadoComposta}, '${descripcionPlanta}', ${status}
+      );`;
+      const nuevaPlanta = await ejecutarConsulta(consultaSQL);
+      res.status(201).json({ data: nuevaPlanta[0] });
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).json({ message: 'Error al crear la planta' });
+    }
+  } 
+  else {
+    res.status(405).json({ message: 'Método no permitido' });
+  }
+}
