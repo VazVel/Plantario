968f11d1d6a9edd554188183c84991f733681227
Raquel Amezcua <raquelh6h@gmail.com>
2025-03-27 21:48:07 -0600

    Avance la pantalla de Registro Planta

--- plantario/pages/index.js
+++ plantario/pages/index.js
@@ -1,9 +1,10 @@
 import Inicio from "../pages/Pantallas/Inicio"; // Importa el componente correctamente
 import InicioSesion from "./Pantallas/inicioSesion.jsx";
 import RegistrationForm from "./Pantallas/registro.jsx";
+import RegistroPlanta from "./Pantallas/RegistroPlanta.jsx"
 
 export default function Home() {
-  return <Inicio />; 
+  return <RegistroPlanta />; 
 
 }
 
