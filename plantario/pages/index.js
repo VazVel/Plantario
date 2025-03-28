02896abecaa634b84eea24a6bd6c46a82c8fde3b
Raquel Amezcua <raquelh6h@gmail.com>
2025-03-28 01:08:15 -0600

    Rutas

--- plantario/pages/index.js
+++ plantario/pages/index.js
@@ -1,7 +1,19 @@
-import Inicio from "./Inicio.js"; // Importa el componente correctamente
+import Link from "next/link";
 
 export default function Home() {
-  return <Inicio />; 
-
+  return (
+    <div>
+      <h1>Página de Inicio</h1>
+      <nav>
+        <ul>
+          <li><Link href="/inicio">Ir a Inicio</Link></li>
+          <li><Link href="/iniciosesion">Iniciar Sesión</Link></li>
+          <li><Link href="/registro">Registro</Link></li>
+          <li><Link href="/registrodetalles">Registro Detalles</Link></li>
+          <li><Link href="/registroplanta">Registro Planta</Link></li>
+          <li><Link href="/base">Base</Link></li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
-
