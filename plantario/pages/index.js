6e01ec214f4257881ee44b109a520e007725b4e6
Raquel Amezcua <raquelh6h@gmail.com>
2025-03-28 00:52:53 -0600

    Minicambio

--- plantario/pages/index.js
+++ plantario/pages/index.js
@@ -1,12 +1,7 @@
 import Inicio from "./Inicio.js"; // Importa el componente correctamente
-import InicioSesion from "./inicioSesion.js";
-import RegistrationForm from "./registro.js";
-import RegistroPlanta from "./RegistroPlanta.js"
-import Base from "./base.js"
-import RegistroDetalles from "./RegistroDetalles.js"
 
 export default function Home() {
-  return <RegistroPlanta />; 
+  return <Inicio />; 
 
 }
 
