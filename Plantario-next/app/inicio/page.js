a45267fd74888c203c83c896396a0411724763f4
Raquel Amezcua <raquelh6h@gmail.com>
2025-04-21 16:14:07 -0600

    Botones de la pantalla de inicio, ya llevan a otras pantallas use el onClick

--- Plantario-next/app/inicio/page.js
+++ Plantario-next/app/inicio/page.js
@@ -1,11 +1,20 @@
-import '../../app/styles/Inicio.css'; // Ajusta la ruta al CSS
+'use client'; 
+
+import { useRouter } from 'next/navigation';
+import '../../app/styles/Inicio.css'; 
 
 const Inicio = () => {
+  const router = useRouter();
   return (
     <div className='Fondo'>
       <h2 className='plantario'>PLANTARIO</h2>
-      <button className='botonRegistrarse'>Registrarse</button>
-      <button className='botonCuenta'>Ya tengo cuenta</button>
+      <button className='botonRegistrarse' onClick={() => router.push('/registro')} >
+         Registrarse
+      </button>
+
+      <button className='botonCuenta' onClick={() => router.push('/iniciosesion')}>
+         Ya tengo cuenta
+      </button>
     </div>
   );
 };
