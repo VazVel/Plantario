86e5027fa4648c36035d8e708ed1c59f2245de88
VazVel <152049637+VazVel@users.noreply.github.com.>
2025-05-12 09:36:39 -0600

    Areglo bugs

--- app/registroplanta/page.js
+++ app/registroplanta/page.js
@@ -2,7 +2,7 @@
 
 import { useRouter } from 'next/navigation';
 import { useState } from "react";
-import "../styles/registroplanta.css";
+import "../styles/RegistroPlanta.css";
 import Swal from 'sweetalert2';
 
 
